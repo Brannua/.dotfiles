862386fba74da0dea7cdd1b1d543c2652a7138ad
Brannua <374453156@qq.com>
2022-07-20 18:47:08 +0800

    update

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,14 @@
+# https://arjanvandergaag.nl/blog/customize-zsh-prompt-with-vcs-info.html
 autoload -U colors && colors
-PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b "
+autoload -U vcs_info
+precmd() { vcs_info }
 
-# git prompt
-# https://github.com/zsh-users/zsh/blob/master/Misc/vcs_info-examples
+# Format the vcs_info_msg_0_ variable
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:git:*' formats "%B<%m%u%c> %{$fg[green]%}%b"
+
+# Set up the prompt
+setopt PROMPT_SUBST
+PROMPT="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$ "
+RPROMPT='$vcs_info_msg_0_'
 
