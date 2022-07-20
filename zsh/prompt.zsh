f0fd0116435325b2a51185fabdf7c4fc50bba4cf
Brannua <374453156@qq.com>
2022-07-20 18:47:50 +0800

    update

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,7 +5,7 @@ precmd() { vcs_info }
 
 # Format the vcs_info_msg_0_ variable
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:git:*' formats "%B<%m%u%c> %{$fg[green]%}%b"
+zstyle ':vcs_info:git:*' formats "%B%m%u%c %{$fg[green]%}%b"
 
 # Set up the prompt
 setopt PROMPT_SUBST
